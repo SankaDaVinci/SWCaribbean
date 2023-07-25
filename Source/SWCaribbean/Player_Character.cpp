358e1810626d1dbcd776021ad98064a7677f3794
Daniel Alves <danielmodeller@hotmail.com>
2023-07-24 21:22:27 -0300

    Initial Commit

--- Source/SWCaribbean/Player_Character.cpp
+++ Source/SWCaribbean/Player_Character.cpp
@@ -2,7 +2,14 @@
 
 
 #include "Player_Character.h"
-
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 // Sets default values
 APlayer_Character::APlayer_Character()
 {
